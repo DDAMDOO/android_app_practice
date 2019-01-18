0db8b56ffe0de2ecefaf50f9c6d1ca6c44327d4e
DDAMDOO <sjl9524@naver.com>
2019-01-18 16:26:27 +0900

    database ex

--- firebase_ex/app/src/main/java/com/ekaen/sjl95/firebase_ex/MainActivity.java
+++ firebase_ex/app/src/main/java/com/ekaen/sjl95/firebase_ex/MainActivity.java
@@ -2,12 +2,30 @@ package com.ekaen.sjl95.firebase_ex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button sendbt;
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button2);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                databaseReference.child("message").push().setValue("2");
+            }
+        });
     }
 }
