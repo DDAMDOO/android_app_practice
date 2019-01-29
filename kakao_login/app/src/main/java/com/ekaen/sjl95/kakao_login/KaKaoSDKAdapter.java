0767919541e828d339a8152a780b8d7ac86209fc
DDAMDOO <sjl9524@naver.com>
2019-01-29 15:46:13 +0900

    kakao login practice

---
+++ kakao_login/app/src/main/java/com/ekaen/sjl95/kakao_login/KaKaoSDKAdapter.java
@@ -0,0 +1,52 @@
+package com.ekaen.sjl95.kakao_login;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.ekaen.sjl95.kakao_login.GlobalApplication;
+import com.kakao.auth.ApprovalType;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.ISessionConfig;
+import com.kakao.auth.KakaoAdapter;
+
+public class KaKaoSDKAdapter extends KakaoAdapter {
+    @Override
+    public ISessionConfig getSessionConfig() {
+        return new ISessionConfig() {
+            @Override
+            public AuthType[] getAuthTypes() {
+                return new AuthType[] {AuthType.KAKAO_ACCOUNT};
+            }
+
+            @Override
+            public boolean isUsingWebviewTimer() {
+                return false;
+            }
+            @Override
+            public ApprovalType getApprovalType() {
+                return ApprovalType.INDIVIDUAL;
+            }
+
+            @Override
+            public boolean isSaveFormData() {
+                return true;
+            }
+        };
+    }
+
+    @Override
+    public IApplicationConfig getApplicationConfig() {
+        return new IApplicationConfig() {
+            @Override
+            public Activity getTopActivity() {
+                return GlobalApplication.getCurrentActivity();
+            }
+
+            @Override
+            public Context getApplicationContext() {
+                return GlobalApplication.getGlobalApplicationContext();
+            }
+        };
+    }
+}
---
+++ kakao_login/app/src/main/java/com/ekaen/sjl95/kakao_login/MainActivity.java
@@ -0,0 +1,85 @@
+package com.ekaen.sjl95.kakao_login;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
+    SessionCallback callback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getHashKey();
+
+        setContentView(R.layout.activity_main);
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        requestMe();
+    }
+    private void getHashKey(){
+        try {                                                        // 패키지이름을 입력해줍니다.
+            PackageInfo info = getPackageManager().getPackageInfo("com.ekaen.sjl95.kakao_login", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("TAG","key_hash="+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void requestMe() {
+        //유저의 정보를 받아오는 함수
+
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Log.e(TAG, "error message=" + errorResult);
+//                super.onFailure(errorResult);
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+
+                Log.d(TAG, "onSessionClosed1 =" + errorResult);
+            }
+
+            @Override
+            public void onNotSignedUp() {
+                //카카오톡 회원이 아닐시
+                Log.d(TAG, "onNotSignedUp ");
+
+            }
+            @Override
+            public void onSuccess(UserProfile result) {
+                Log.e("UserProfile", result.toString());
+                Log.e("UserProfile", result.getId() + "");
+            }
+        });
+    }
+}
+
+
